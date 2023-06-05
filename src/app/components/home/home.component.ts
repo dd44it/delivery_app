cd78ed586051a93b755d13e2b16d45c90e2abc6a
dd44it <12nelson13@gmail.com>
2023-06-05 20:34:37 +0300

    feat: add dynamic data shops and products

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ShopService } from "src/app/services/shop.service";
+import { Shop } from "src/app/Shop";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html'
+  selector: "app-home",
+  templateUrl: "./home.component.html",
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  shops: Shop[] = [];
+  error: string = "";
 
+  constructor(private shopService: ShopService) {}
+
+  ngOnInit(): void {
+    this.getAllShops();
+  }
+
+  getAllShops(): void {
+    this.shopService.getAllShops().subscribe(
+      (data: Shop[]) => {
+        this.shops = data;
+        console.log(this.shops);
+      },
+      (error: any) => {
+        this.error =
+          "An error occurred while fetching shops. Please try again later.";
+        console.error(error);
+      }
+    );
+  }
 }
