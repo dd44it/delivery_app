589ddc041202c1ae691ccd94f445f37b4f190d91
dd44it <12nelson13@gmail.com>
2023-06-06 11:38:37 +0300

    feat: add cart items to cartComponent

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/Shop';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: 'cart.component.html'
 })
-export class CartComponent {
+export class CartComponent implements OnInit {
+  products: Product[] = []
+
+  constructor(private cartService: CartService){}
+
+  ngOnInit(): void {
+    this.products = this.cartService.getCart()
+    console.log(this.products)
+    
+  }
 
 }
