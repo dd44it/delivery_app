cb7789756a6043bdb3a455203a99e7ce11a4a58a
dd44it <12nelson13@gmail.com>
2023-06-06 16:23:24 +0300

    feat: add send method to db

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,20 +1,49 @@
-import { Component, OnInit } from '@angular/core';
-import { Product } from 'src/app/Shop';
-import { CartService } from 'src/app/services/cart.service';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder } from "@angular/forms";
+import { Product } from "src/app/Shop";
+import { CartService } from "src/app/services/cart.service";
 
 @Component({
-  selector: 'app-cart',
-  templateUrl: 'cart.component.html'
+  selector: "app-cart",
+  templateUrl: "cart.component.html",
 })
 export class CartComponent implements OnInit {
-  products: Product[] = []
+  products: Product[] = [];
 
-  constructor(private cartService: CartService){}
+  checkoutForm = this.formBuilder.group({
+    name: "",
+    email: "",
+    phone: "",
+    address: "",
+    products: this.formBuilder.array(this.products),
+    finalPrice: 0,
+  });
+
+  constructor(
+    private cartService: CartService,
+    private formBuilder: FormBuilder
+  ) {}
 
   ngOnInit(): void {
-    this.products = this.cartService.getCart()
-    console.log(this.products)
-    
+    this.products = this.cartService.getCart();
   }
 
+  onSubmit(): void {
+    const finalPrice = this.products?.map(item => item.count * item.price).reduce( (prevVal, curVal) => prevVal + curVal, 0);
+    const formData = {
+      ...this.checkoutForm.value,
+      products: this.products.map((product: Product) => {
+        return {
+          id: product._id,
+          name: product.name,
+          price: product.price,
+          count: product.count,
+        };
+      }),
+      finalPrice,
+    };
+    console.warn("Your order has been submitted", formData);
+
+    this.cartService.sendDataToDB(formData);
+  }
 }
--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { BehaviorSubject } from "rxjs";
 import { Product } from "../Shop";
 
@@ -8,8 +9,9 @@ import { Product } from "../Shop";
 export class CartService {
   private cart: Product[] = [];
   private cartItemCount = new BehaviorSubject<number>(0);
+  private orderUrl = "http://localhost:5000/api/order/";
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   getCartItemCount(): BehaviorSubject<number> {
     return this.cartItemCount;
@@ -24,11 +26,34 @@ export class CartService {
       product.count += 1;
       this.cart.push(product);
     }
-    const sumProducts = this.cart.reduce((prevIt, currIt) => prevIt + currIt.count, 0)
+    const sumProducts = this.cart.reduce(
+      (prevIt, currIt) => prevIt + currIt.count,
+      0
+    );
     this.cartItemCount.next(sumProducts);
   }
 
   getCart(): Product[] {
     return this.cart;
+  }
+
+  sendDataToDB(data: any): void {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        "Content-Type": "application/json",
+      }),
+    };
+
+    this.http.post<any>(this.orderUrl, data, httpOptions).subscribe(
+      (response) => {
+        console.log("Data sent to MongoDB successfully:", response);
+        // Perform any additional actions after successful data submission
+        // return
+      },
+      (error) => {
+        console.error("Error occurred while sending data to MongoDB:", error);
+        // Handle any errors that occurred during data submission
+      }
+    );
   }
 }
