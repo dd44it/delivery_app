c06644b2e69f6277a2da8ec87664a49f44248093
dd44it <12nelson13@gmail.com>
2023-06-06 20:09:04 +0300

    init: add cors

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const errorHandler = require("./middleware/errorHandler");
 const loadDefaultData = require("./config/dbConnection");
 
 const app = express();
 const port = 5000;
-
+app.use(cors());
 app.use(express.json());
 loadDefaultData();
 app.use("/api/shops", require("./routes/shopRoutes"));
