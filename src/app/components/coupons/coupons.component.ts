61d2171202188120618685a5ce615e43e59eac1b
dd44it <12nelson13@gmail.com>
2023-06-21 21:32:24 +0300

    feat: display coupons in page

--- src/app/components/coupons/coupons.component.ts
+++ src/app/components/coupons/coupons.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
-import { Coupon } from 'src/app/Shop';
+import { Component, OnInit } from "@angular/core";
+import { Coupon } from "src/app/Shop";
+import { CouponService } from "src/app/services/coupon.service";
 
 @Component({
-  selector: 'app-coupons',
-  templateUrl: 'coupons.component.html'
+  selector: "app-coupons",
+  templateUrl: "coupons.component.html",
 })
-export class CouponsComponent {
+export class CouponsComponent implements OnInit {
   coupons: Coupon[] = [];
   resultResponse = "";
+  error: string = "";
 
+  constructor(private couponService: CouponService) {}
+
+  ngOnInit(): void {
+    this.getAllCoupons();
+  }
+
+  getAllCoupons(): void {
+    this.couponService.getAllCoupons().subscribe(
+      (data: Coupon[]) => {
+        this.coupons = data;
+      },
+      (error: any) => {
+        this.error =
+          "An error occurred while fetching shops. Please try again later.";
+        console.error(error);
+      }
+    );
+  }
+
+  onCopyCode(code: string): void {
+    navigator.clipboard.writeText(code);
+  }
 }
