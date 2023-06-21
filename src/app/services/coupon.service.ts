0271b9facb2a1a85937b321dd5ac58ae5d37e4f7
dd44it <12nelson13@gmail.com>
2023-06-21 21:32:00 +0300

    feat: add service to get all coupons

---
+++ src/app/services/coupon.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { Observable, throwError } from "rxjs";
+import { catchError } from "rxjs/operators";
+import { Coupon } from "src/app/Shop";
+
+@Injectable({
+  providedIn: "root",
+})
+export class CouponService {
+  private apiURL = "http://localhost:5000/api/coupons";
+
+  constructor(private http: HttpClient) {}
+
+  getAllCoupons(): Observable<Coupon[]> {
+    return this.http.get<Coupon[]>(this.apiURL).pipe(
+      catchError((error: HttpErrorResponse) => {
+        let errorMessage = "Unknown error occurred";
+        if (error.error instanceof ErrorEvent) {
+          // Client-side error
+          errorMessage = `Error: ${error.error.message}`;
+        } else {
+          // Server-side error
+          errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+        }
+        console.error(errorMessage);
+        return throwError(errorMessage);
+      })
+    );
+  }
+}
