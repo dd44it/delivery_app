62a42479570c27ec52a7611768e8798fb417d722
dd44it <12nelson13@gmail.com>
2023-06-26 21:52:52 +0300

    feat: add nelify function fetch-address-from-location

---
+++ netlify/functions/fetch-address-from-location.js
@@ -0,0 +1,26 @@
+const axios = require('axios')
+
+const handler = async (event) => {
+  console.log(event.queryStringParameters)
+  const { lat, lon } = event.queryStringParameters;
+  console.log(lat, lon)
+  console.log(typeof lat, typeof lon)
+  const API_SECRET = process.env.API_MAP;
+  const url = `https://api.geoapify.com/v1/geocode/reverse?lat=${+lat}&lon=${+lon}&format=json&apiKey=${API_SECRET}&lang=uk`;
+
+  try {
+    const { data } = await axios.get(url);
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    }
+  } catch (error) {
+    const { status, statusText, headers, data } = error.response;
+    return {
+      statusCode: status,
+      body: JSON.stringify({ status, statusText, headers, data }),
+    }
+  }
+}
+
+module.exports = { handler }
