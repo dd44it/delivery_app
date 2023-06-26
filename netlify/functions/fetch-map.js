c43eeb4b0e01d8f65b9fa9c2b921c9890a4ffe0b
dd44it <12nelson13@gmail.com>
2023-06-26 21:51:44 +0300

    feat: add nelify function fetch-map

---
+++ netlify/functions/fetch-map.js
@@ -0,0 +1,22 @@
+const axios = require('axios')
+
+const handler = async (event) => {
+  const API_SECRET = process.env.API_MAP;
+  const url =`https://api.geoapify.com/v1/ipinfo?apiKey=${API_SECRET}`;
+
+  try {
+    const { data } = await axios.get(url);
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    }
+  } catch (error) {
+    const { status, statusText, headers, data } = error.response;
+    return {
+      statusCode: status,
+      body: JSON.stringify({ status, statusText, headers, data }),
+    }
+  }
+}
+
+module.exports = { handler }
