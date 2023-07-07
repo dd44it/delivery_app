2d862632e6329aeedad3281790a6277ad04d8f60
dd44it <12nelson13@gmail.com>
2023-07-07 21:53:21 +0300

    refactor: add call to netlify function update count code

--- src/app/services/coupon.service.ts
+++ src/app/services/coupon.service.ts
@@ -13,6 +13,8 @@ export class CouponService {
   private urlGetCoupons = "/.netlify/functions/fetch-coupons";
   // get coupon by code
   private urlGetCoupon = "/.netlify/functions/fetch-get-coupon-code";
+  // update coupon count 
+  private urlUpdateCouponCount = "/.netlify/functions/fetch-update-coupon-count";
 
   constructor(private http: HttpClient) {}
 
@@ -91,6 +93,7 @@ export class CouponService {
     ); 
   }
 
+  // update coupon by code from local db with node.js
   // updateSelectedCouponCode(id: string): Observable<any>{
   //   const url = `${this.apiURL}/${id}`
   //   return this.http.put<Coupon[]>(url, id).pipe(
@@ -108,4 +111,24 @@ export class CouponService {
   //     })
   //   ); 
   // }
+
+  // update coupon by code from mongo db cluster with netlify function
+  updateSelectedCouponCode(id: string): Observable<any>{
+    let params = new HttpParams()
+    params = params.append('id', id);
+    return this.http.get<Coupon[]>(this.urlUpdateCouponCount, { params }).pipe(
+      catchError((error: HttpErrorResponse) => {
+        let errorMessage = "Unknown error occurred";
+        if (error.error instanceof ErrorEvent) {
+          // Client-side error
+          errorMessage = `Error: ${error.error.message}`;
+        } else {
+          // Server-side error
+          errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+        }
+        console.error(errorMessage);
+        return throwError(errorMessage);
+      })
+    ); 
+  }
 }
